dec45ab636385330d881f6d532b9b646f03395fd
claym <clay@pfd.net>
2015-06-24 16:35:24 -0400

    fixed removed dependencies

--- src/main/java/io/listened/api/controller/PodcastController.java
+++ src/main/java/io/listened/api/controller/PodcastController.java
@@ -1,6 +1,5 @@
 package io.listened.api.controller;
 
-import io.listened.api.configuration.QueueConfig;
 import io.listened.api.model.Podcast;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +18,9 @@ public class PodcastController {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
-    @RequestMapping(method= RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     private Podcast submit(@RequestParam(value = "feedUrl", required = true) String feedUrl) {
-        rabbitTemplate.convertAndSend(QueueConfig.podcastSubmitQueueName, feedUrl);
-        rabbitTemplate.convertAndSend(QueueConfig.podcastUpdateQueue, feedUrl);
+
         return new Podcast();
     }
 
