92811109a8fc42858dcf9a9e4e9bc00b18ee5e57
Clay Mitchell <clay@pfd.net>
2015-06-27 22:18:48 -0400

    Added podcast saving functionality

--- src/main/java/io/listened/api/exception/FeedExistsException.java
+++ src/main/java/io/listened/api/exception/FeedExistsException.java
@@ -2,6 +2,7 @@ package io.listened.api.exception;
 
 /**
  * Created by Clay on 6/25/2015.
+ * Exception for submission of feed that already exists in the database
  */
 public class FeedExistsException extends RuntimeException {
     public FeedExistsException(String feedUrl) {
