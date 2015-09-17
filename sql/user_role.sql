fac4765c0958786891eb70ac5599bdfa641afd49
claym <clay@pfd.net>
2015-09-17 17:14:39 -0400

    user role script

---
+++ sql/user_role.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "MY_TABLE" (
+  user_id int8,
+  role_id varchar
+);
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_ADMIN');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_MODERATOR');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_USER_UNVERIFIED');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_USER');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_SUPER_ADMIN');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8260, 'ROLE_OWNER');
+INSERT INTO "MY_TABLE"(user_id, role_id) VALUES (8261, 'ROLE_USER');
